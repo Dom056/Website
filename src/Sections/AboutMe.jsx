04f3c464d3aa5f3a80bf0d783b79c3f2ce309c2f
Dom056 <dominicdawson99@gmail.com>
2026-02-09 15:54:07 +0000

    FINALLY FIXED ABOUT ME SECTION!!!

--- src/Sections/AboutMe.jsx
+++ src/Sections/AboutMe.jsx
@@ -12,7 +12,7 @@ import MongoDB from "../Assets/mongodb.png"
 import Express from "../Assets/express.png"
 import CSS from "../Assets/css-3.png"
 
-import "./Sections.css";
+import "./AboutMe.css";
 
 function AboutMe() {  
     return (
@@ -22,8 +22,8 @@ function AboutMe() {
             <div className="about-text">
             <p>I'm a Second year Software engineering student at Bournemouth University with a focus on full stack web development. </p>
             <p>I've always been driven by creating practical solutions to real world problems and have worked across every stage of web development. From designing UI wireframes, building frontend components and developing backend functionality. To testing, QA and launch. I pride myself in producing high-quality work for every project I undertake. </p>
-            <p>Outside of coding I'm constantly working on DIY tech projects, whether it be tinkering with raspberry pis to create a home assistant, dabbling in electronics or 3D printing custom housings. I'm always eager to learn new skills to keep myself busy.</p>
-            <p>Currently looking for an internship for internships so I can get some real world experience and to learn from experienced software engineers as I develop my skill set </p >
+            <p>Outside of coding I'm constantly working on DIY tech projects, whether it be tinkering with raspberry pis, dabbling in electronics or 3D printing custom housings. I'm always eager to learn new skills to keep myself busy.</p>
+            <p>Currently looking for an internship for internships so I can get some real world experience and to learn from experienced software engineers as I develop my skill set. </p >
             </div>
             </div>
                 <div className="skills">    
--- src/Sections/Project.jsx
+++ src/Sections/Project.jsx
@@ -1,4 +1,4 @@
-import './Sections.css';
+import './Project.css';
 
 function Project () {
     return (
